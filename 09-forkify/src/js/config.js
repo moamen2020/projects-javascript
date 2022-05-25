0b0044e5642f57693779ea3ec8c7b29443e9ec66
moamen2020 <m01144944981@gmail.com>
2022-05-25 22:04:48 +0200

    project 9 - advanced project & you need fixed problem

---
+++ 09-forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3e55be22-0322-497f-8f08-b4c6d13211f5';
+export const MODAL_CLOSE_SEC = 2.5;
